40ca0a896058d163ffc2e86c9d4ac43dea888ff4
cyrkov <cyrkov@jinr.ru>
2017-07-19 20:17:45 +0300

    Some more refactoring for mconvd

--- mconvd.cc
+++ mconvd.cc
@@ -5,12 +5,9 @@
 //         mconvd
 //
 //ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-void mconvd(const int n, double z[], double r[], double pl0[], int * indf) {
+void mconvd(const int n, double * const z, double * const r, double * const pl0, int * indf) {
   const double am = 3.4e138;
   const double rp = 5.0e-14;
-  --pl0;
-  --r;
-  --z;
   if (n < 1) return;
   double aps = am / n;
   double ap = 1.0e0 / aps;
@@ -18,27 +15,27 @@ void mconvd(const int n, double z[], double r[], double pl0[], int * indf) {
   for (int i = 1, ir = 0; i <= n; ++i) {
     do {
       ++ir;
-    } while (pl0[ir] <= 0.0e0);
+    } while (pl0[ir-1] <= 0.0e0);
     int ni = i * (i - 1) / 2;
     int ii = ni + i;
-    if (z[ii] <= rp * fabs(r[ir]) || z[ii] <= ap) {
-      pl0[ir] = -2.0e0;
-      r[ir] = 0.0e0;
+    if (z[ii-1] <= rp * fabs(r[ir-1]) || z[ii-1] <= ap) {
+      pl0[ir-1] = -2.0e0;
+      r[ir-1] = 0.0e0;
       *indf = ir - 1;
       return;
     }
-    z[ii] = 1.0e0 / sqrt(z[ii]);
+    z[ii-1] = 1.0e0 / sqrt(z[ii-1]);
     for (int nl = ii - 1; nl - ni > 0; --nl) {
-      z[nl] *= z[ii];
-      if (fabs(z[nl]) >= aps) {
+      z[nl-1] *= z[ii-1];
+      if (fabs(z[nl-1]) >= aps) {
         int ir = 0;
         for (int i = 1; i <= i + nl - ii; ++i) {
           do {
             ++ir;
-          } while (pl0[ir] <= 0.0e0);
+          } while (pl0[ir-1] <= 0.0e0);
         }
-        pl0[ir] = -2.0e0;
-        r[ir] = 0.0e0;
+        pl0[ir-1] = -2.0e0;
+        r[ir-1] = 0.0e0;
         *indf = ir - 1;
         return;
       }
@@ -50,13 +47,13 @@ void mconvd(const int n, double z[], double r[], double pl0[], int * indf) {
       int nk = k * (k - 1) / 2;
       int nl = nk;
       int kk = nk + i;
-      double d = z[kk] * z[ii];
-      double c = d * z[ii];
+      double d = z[kk-1] * z[ii-1];
+      double c = d * z[ii-1];
       int l = k;
       do {
         int ll = nk + l;
         int li = nl + i;
-        z[ll] -= z[li] * c;
+        z[ll-1] -= z[li-1] * c;
         --l;
         nl -= l;
       } while (l - i > 0);
@@ -64,10 +61,10 @@ void mconvd(const int n, double z[], double r[], double pl0[], int * indf) {
       while (l > 0) {
         int ll = nk + l;
         int li = ni + l;
-        z[ll] -= z[li] * d;
+        z[ll-1] -= z[li-1] * d;
         --l;
       }
-      z[kk] = -c;
+      z[kk-1] = -c;
     } while (k - i - 1 > 0);
   }
   for (int i = 1; i <= n; ++i) {
@@ -78,11 +75,11 @@ void mconvd(const int n, double z[], double r[], double pl0[], int * indf) {
       for (int l = k; l <= n; ++l) {
         int li = nl + i;
         int lk = nl + k;
-        d += z[li] * z[lk];
+        d += z[li-1] * z[lk-1];
         nl += l;
       }
       ki = k * (k - 1) / 2 + i;
-      z[ki] = d;
+      z[ki-1] = d;
     }
   }
 }
--- mconvd.h
+++ mconvd.h
@@ -5,4 +5,8 @@ inline int ind(const int i, const int j) {
    if (i >= j) return (i*(i+1)/2 + j);
    else        return (j*(j+1)/2 + i);
 }
-void mconvd(const int n, double z[], double r[], double pl0[], int * indf);
+void mconvd( const int n,
+             double * const z,
+             double * const r,
+             double * const pl0,
+             int * indf );
